91f991b23615aac29282b00e26412f153cae2dc1
prottoy <prottoyroy01@gmail.com>
2020-07-04 21:37:21 +0600

    splash to main page

--- app/src/main/java/com/example/tictactoe/SplashScreen.java
+++ app/src/main/java/com/example/tictactoe/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -17,17 +18,19 @@ public class SplashScreen extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
         progressBar =(ProgressBar) findViewById(R.id.progressbarid);
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 doWork();
+                startApp();
             }
         });
         thread.start();
     }
     public void doWork(){
-        for (progress=20; progress<=100; progress=progress)
+        for (progress=20; progress<=100; progress=progress+20)
         {
             try {
                 Thread.sleep(1000);
@@ -37,5 +40,10 @@ public class SplashScreen extends AppCompatActivity {
             }
         }
 
+    }
+    public void startApp(){
+        Intent intent= new Intent(SplashScreen.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
