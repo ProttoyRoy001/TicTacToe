21e4cdc8ec1f212cec4c04e25559cf122b7f2b19
prottoy <prottoyroy01@gmail.com>
2020-07-04 21:07:07 +0600

    splash screen

---
+++ app/src/main/java/com/example/tictactoe/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        progressBar =(ProgressBar) findViewById(R.id.progressbarid);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+            }
+        });
+        thread.start();
+    }
+    public void doWork(){
+        for (progress=20; progress<=100; progress=progress)
+        {
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
